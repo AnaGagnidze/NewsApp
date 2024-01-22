e1da65bc5f87e019b2e8a2e77b53ec3f9b2aa29d
anagagnidze <agagnidze@tbcbank.com.ge>
2024-01-22 17:19:18 +0400

    onboarding finished

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,15 +1,22 @@
 package com.loc.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.loc.newsapp.presentation.Dimens.IndicatorWidth
+import com.loc.newsapp.presentation.Dimens.MediumPadding2
+import com.loc.newsapp.presentation.common.NewsButton
+import com.loc.newsapp.presentation.common.NewsTextButton
 import com.loc.newsapp.presentation.onboarding.components.OnBoardingPage
+import com.loc.newsapp.presentation.onboarding.components.PageIndicator
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -32,5 +39,47 @@ fun OnBoardingScreen() {
         HorizontalPager(state = pagerState) { index ->
             OnBoardingPage(page = pages[index])
         }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MediumPadding2)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(
+                pageSize = pages.size,
+                selectedPage = pagerState.currentPage,
+                modifier = Modifier.width(
+                    IndicatorWidth
+                )
+            )
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val scope = rememberCoroutineScope()
+
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(text = buttonState.value[0]) {
+                        scope.launch {
+                            pagerState.animateScrollToPage(page = pagerState.currentPage - 1)
+                        }
+                    }
+                }
+                NewsButton(text = buttonState.value[1]) {
+                    scope.launch {
+                        if (pagerState.currentPage == 3) {
+                            // todo
+                        } else {
+                            pagerState.animateScrollToPage(page = pagerState.currentPage + 1)
+                        }
+                    }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.weight(0.5f))
     }
 }
